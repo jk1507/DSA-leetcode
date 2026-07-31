4f2d952d8a4c4512533e1ecccb08c616c248a053
Jaya Krishna <boyijayakrishna2006@gmail.com>
2026-07-31 12:28:20 -0400

    Time: 0 ms (100.00%) | Memory: 100.5 MB (6.84%) - LeetSync

--- 932-monotonic-array/monotonic-array.cpp
+++ 932-monotonic-array/monotonic-array.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        // int increase=1,decrease=1;
-        // if(nums.size()==1) return true;
-        // for(int i=0;i<nums.size()-1;i++){
-        //     if(nums[i]>=nums[i+1]){
-        //         decrease++;
-        //     }
-        // }
-        // for(int i=0;i<nums.size()-1;i++){
-        //     if(nums[i]<=nums[i+1]){
-        //         increase++;
-        //     }
-        // if(increase==nums.size()|| decrease==nums.size()){
-        //     return true;
-        // }
-        // }
-        // return false;
-        bool incr=true,decre=true;
+        int increase=1,decrease=1;
+        if(nums.size()==1) return true;
         for(int i=0;i<nums.size()-1;i++){
-            if(nums[i+1]<nums[i]) incr=false;
-            if(nums[i+1]>nums[i]) decre=false;
-            
+            if(nums[i]>=nums[i+1]){
+                decrease++;
+            }
         }
-        return incr||decre;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]<=nums[i+1]){
+                increase++;
+            }
+        if(increase==nums.size()|| decrease==nums.size()){
+            return true;
+        }
+        }
+        return false;
+        // bool incr=true,decre=true;
+        // for(int i=0;i<nums.size()-1;i++){
+        //     if(nums[i+1]<nums[i]) incr=false;
+        //     if(nums[i+1]>nums[i]) decre=false;
+            
+        // }
+        // return incr||decre;
     }
 };
