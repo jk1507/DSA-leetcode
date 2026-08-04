c775b2a2f0a7afa8e3317a754168484bdf635ddc
Jaya Krishna <boyijayakrishna2006@gmail.com>
2026-08-04 12:12:09 -0400

    Time: 0 ms (100.00%) | Memory: 8.2 MB (22.01%) - LeetSync

---
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        vector<int> factorStore;
+        for(int i=1;i<=n;i++){
+            if(n%i==0) {
+                factorStore.push_back(i);
+            }
+        }
+        sort(factorStore.begin(),factorStore.end());
+        int size=factorStore.size();
+        if(size<k) return -1;
+        return factorStore[k-1];
+    }
+};
