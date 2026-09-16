935bb18cd1a374c25266dfd75bf69d45e0a8a40d
Jaya Krishna <boyijayakrishna2006@gmail.com>
2026-09-16 12:06:50 -0400

    Time: 7 ms (11.24%) | Memory: 15 MB (18.84%) - LeetSync

---
+++ 1674-minimum-operations-to-make-array-equal/minimum-operations-to-make-array-equal.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minOperations(int n) {
+      int operations=0;
+      vector<int> x(n);
+      for(int i=0;i<n;i++){
+        x[i]=(2*i)+1;
+      }
+      for(int i=0;i<x.size()/2;i++){
+        if(x[i]<n){
+            int sum=n-x[i];
+            operations+=sum;
+        }
+      }
+      return operations;
+    }
+};
